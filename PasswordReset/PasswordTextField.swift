1666fb2e8bc65dc5f6f769ddc2d18f8bb8ebd4a1
Edwin Cardenas <ecardenas@lwolf.com>
2025-07-31 17:07:23 -0500

    feat: restrict space character usage for password text field

--- PasswordReset/PasswordTextField.swift
+++ PasswordReset/PasswordTextField.swift
@@ -206,6 +206,18 @@ extension PasswordTextField: UITextFieldDelegate {
         return true
     }
 
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let replacementStringHasWhiteSpace = string.rangeOfCharacter(
+            from: CharacterSet.whitespaces
+        )
+
+        return replacementStringHasWhiteSpace == nil
+    }
+
 }
 
 // MARK: - Actions
